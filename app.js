68b95e10fc7af14255b7e2027de8b3337dca5249
Robinson Cuzco <ingrcuzco@gmail.com>
2025-02-20 15:54:45 -0500

    actual

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axion = require('axios');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;//para subir en azure port caso contrario port 3000
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
